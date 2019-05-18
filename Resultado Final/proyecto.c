db41eb9017634370d2d91cdaa769c87dd721c424
oelshami <47811531+oelshami@users.noreply.github.com>
2019-05-18 22:17:54 +0200

    Letrero

--- Resultado Final/proyecto.c
+++ Resultado Final/proyecto.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
 #include "GEC.h"
-
+void pintar_espacio(int veces);
 int main(){
 	system("color FD");
-	int i;
-	printf("\n\tBienvenido a GEC");
+	int i,j;
+	float t1,t2;
+	//////Letrero////////
+	for(i=1,j=0;i<70;i++){
+
+		printf("\n\n\n\n\n\n\n\n"); 
+		pintar_espacio(j); 
+		printf(" XXXXXX     XXXXXXX    XXXXX\n");
+		pintar_espacio(j);
+		printf("XXXXXXXX    XXXXXXX   XXXXXXX  \n");
+		pintar_espacio(j);
+		printf("XX    XX    XX        XX   XX  \n");
+		pintar_espacio(j);
+		printf("XX          XXXX      XX\n");
+		pintar_espacio(j);
+		printf("XX  XXXX    XXXX      XX\n");
+		pintar_espacio(j);
+		printf("XX  XXXX    XX        XX\n");
+		pintar_espacio(j);
+		printf("XX    XX    XX        XX   XX\n");
+		pintar_espacio(j);
+		printf("XXXXXXXX    XXXXXXX   XXXXXXX\n");
+		pintar_espacio(j);
+		printf(" XXXXX   X  XXXXXXX  X  XXXX   X\n");
+		t2=t1=clock();
+		j++;
+		while((t2-t1)/CLOCKS_PER_SEC<0.1){
+			t2=clock();
+		}
+		system("cls");
+	}
+	
 	getch();
 	while(1){
 		system("color FD");
@@ -26,10 +56,16 @@ int main(){
 				Ahorcado();
 			break;
 			default:
+				printf("\n\nHasta la proxima");
 				return 1;
 		}
 		system("cls");
 	}
 	return 0;
+}
+void pintar_espacio(int veces){
+	int i;
+	for(i=1;i<=veces;i++)
+		printf(" ");
 }
 
